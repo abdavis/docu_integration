7dfd2655c6637159294a36373f7fe244aa7876e0
Andrew Davis <abdavis7@gmail.com>
2022-11-17 16:55:44 -0700

    remove tls code from repository I'll be switching to a reverse proxy instead.

--- src/create_server.rs
+++ src/create_server.rs
@@ -1,6 +1,6 @@
-use axum_server::{tls_rustls::RustlsConfig, Handle};
-use std::{net::SocketAddr, path::PathBuf, time::Duration};
-use tokio::task;
+//use axum_server::tls_rustls::RustlsConfig;
+//use std::{net::SocketAddr, path::PathBuf, time::Duration};
+use tokio::{sync::oneshot, task};
 
 use crate::{db, Config};
 
@@ -19,13 +19,6 @@ pub async fn run(
 	completed_processor_tx: async_channel::Sender<()>,
 	shutdown_signal: tokio::sync::broadcast::Receiver<()>,
 ) {
-	let tls_config = RustlsConfig::from_pem_file(
-		PathBuf::from("cert/cert.pem"),
-		PathBuf::from("cert/key.pem"),
-	)
-	.await
-	.expect("Unable to load certificates");
-
 	let app = webhook::create_routes(&config, db_wtx.clone(), completed_processor_tx)
 		.merge(login::create_routes(db_wtx.clone(), db_rtx.clone()).await)
 		.merge(admin::create_routes(db_wtx.clone(), db_rtx.clone()))
@@ -35,22 +28,23 @@ pub async fn run(
 			ws_handler_tx,
 		));
 
-	let handle = Handle::new();
+	let (tx, rx) = oneshot::channel();
 
-	task::spawn(graceful_shutdown(handle.clone(), shutdown_signal));
+	task::spawn(graceful_shutdown(tx, shutdown_signal));
 
-	let addr = SocketAddr::from(([0, 0, 0, 0], 8081));
-	axum_server::bind_rustls(addr, tls_config)
-		.handle(handle)
+	axum::Server::bind(&([127, 0, 0, 1], 8081).into())
 		.serve(app.into_make_service())
+		.with_graceful_shutdown(async {
+			rx.await.ok();
+		})
 		.await
 		.expect("Failed to start server");
 }
 
 async fn graceful_shutdown(
-	handle: Handle,
+	handle: oneshot::Sender<()>,
 	mut shutdown_signal: tokio::sync::broadcast::Receiver<()>,
 ) {
 	shutdown_signal.recv().await.unwrap_or_default();
-	handle.graceful_shutdown(Some(Duration::from_secs(10)));
+	handle.send(()).unwrap_or_default();
 }
